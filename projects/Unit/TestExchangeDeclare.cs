73e3f513210e69c11b140cb81eaacd8191843628
Luke Bakken <luke@bakken.io>
2023-05-11 10:04:20 -0700

    Implement async for simple AMQP methods
    Related to:
    * #1345
    * #1308
    * #970
    * #843

--- projects/Unit/TestExchangeDeclare.cs
+++ projects/Unit/TestExchangeDeclare.cs
@@ -32,7 +32,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading;
-
+using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -80,6 +80,39 @@ namespace RabbitMQ.Client.Unit
 
             Assert.Null(nse);
             _channel.ExchangeDelete(x);
+        }
+
+        [Fact]
+        public async void TestConcurrentExchangeDeclareAsync()
+        {
+            string x = GenerateExchangeName();
+            var rnd = new Random();
+
+            var ts = new List<Task>();
+            NotSupportedException nse = null;
+            for (int i = 0; i < 256; i++)
+            {
+                async Task f()
+                {
+                    try
+                    {
+                        // sleep for a random amount of time to increase the chances
+                        // of thread interleaving. MK.
+                        await Task.Delay(rnd.Next(5, 50));
+                        await _channel.ExchangeDeclareAsync(x, "fanout", false, false, null);
+                    }
+                    catch (NotSupportedException e)
+                    {
+                        nse = e;
+                    }
+                }
+                var t = Task.Run(f);
+                ts.Add(t);
+            }
+
+            await Task.WhenAll(ts);
+            Assert.Null(nse);
+            await _channel.ExchangeDeleteAsync(x);
         }
     }
 }
